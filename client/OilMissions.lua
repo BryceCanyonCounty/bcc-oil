1b8630883990892529cae636ea4d5dd01a06e09a
Quiteck1983 <53975057+Quiteck1983@users.noreply.github.com>
2024-09-21 11:09:12 +0200

    Update OilMissions.lua
    Add Manager-NPC-Coords to Config

--- client/OilMissions.lua
+++ client/OilMissions.lua
@@ -158,7 +158,7 @@ end)
 
 ----------------------------Oil Mission Tables----------------------
 OilWagonTable = {} --creates the table
-OilWagonTable.ManagerSpawn = {x = 498.05, y = 672.98, z = 121.04, h = 73.92} --This is where the manager npc will spawn(Do not change!!)
+OilWagonTable.ManagerSpawn = Config.OilManagerNPC --This is where the manager npc will spawn(Do not change!!)
 OilWagonTable.WagonSpawnCoords = Config.OilandSupplyWagonSpawn --this is the x y z and heaing where the wagons will spawn
 
 --This is the table that the initial wagon fill spot will be
